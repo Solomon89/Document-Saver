ea239c3194566e27bc0d33169bc012640429f8c0
Дмитрий Владимирович Соломонов <dvsolomonov@npo-nhp.com>
2019-12-28 11:44:01 +0300

    Добавьте файлы проекта.

---
+++ DocumentSaver/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DocumentSaver.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace DocumentSaver.Controllers
+{
+    public class HomeController : Controller
+    {
+        FileSaverDataContext _context;
+        IWebHostEnvironment _appEnvironment;
+
+        public HomeController(FileSaverDataContext context, IWebHostEnvironment appEnvironment)
+        {
+            _context = context;
+            _appEnvironment = appEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Files.ToList());
+        }
+        
+        public async Task<IActionResult> AddFile(IFormFile uploadedFile)
+        {
+            if (uploadedFile != null)
+            {
+                // путь к папке Files
+                string path = "/Files/" + uploadedFile.FileName;
+                // сохраняем файл в папку Files в каталоге wwwroot
+                using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))
+                {
+                    await uploadedFile.CopyToAsync(fileStream);
+                }
+                FileModel file = new FileModel { Name = uploadedFile.FileName, Path = path };
+                _context.Files.Add(file);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ DocumentSaver/Models/FileSaverDataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocumentSaver.Models
+{
+    public class FileSaverDataContext:DbContext
+    {
+        public DbSet<FileModel> Files { get; set; }
+        public FileSaverDataContext(DbContextOptions<FileSaverDataContext> options)
+            : base(options)
+        { }
+    }
+}
